53b72ab706507d7c64ef9c8842450b2cc73a16ec
Angramme <34948473+Angramme@users.noreply.github.com>
2019-07-20 23:32:55 +0200

    Create generator.js
    a simple generator for timings of beats

---
+++ generator.js
@@ -0,0 +1,20 @@
+
+
+function getPolyrthytm(a, b, time){
+  var dA = time / a;
+  var dB = time / b;
+  
+  return {
+    a: generateTimings(dA, time),
+    b: generateTimings(dB, time)
+  }
+}
+
+function generateTimings(d, time){
+  var timings = []
+  var t = 0
+  while(t < time){
+    timings.push(t)
+    t += d
+  }
+}
